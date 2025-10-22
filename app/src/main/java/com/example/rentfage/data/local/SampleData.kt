d9b803a06a3b0a5ebc6ded7534e33fb041db37ee
Fran <ger.santana@duocuc.cl>
2025-10-22 09:11:06 -0300

    modificaion SampleData.kt

--- app/src/main/java/com/example/rentfage/data/local/SampleData.kt
+++ app/src/main/java/com/example/rentfage/data/local/SampleData.kt
@@ -10,8 +10,8 @@ data class Casa(
 
 // la lista de casas.
 val casasDeEjemplo = listOf(
-    Casa(1, "UF 28.500", "Av. Vitacura, Vitacura, Santiago", "4 hab | 5 baños | 450 m²"),
-    Casa(2, "UF 35.000", "Camino La Dehesa, Lo Barnechea, Santiago", "5 hab | 6 baños | 600 m² | Piscina"),
-    Casa(3, "UF 26.500", "San Damián, Las Condes, Santiago", "4 hab | 4 baños | 500 m² | Jardín amplio"),
-    Casa(4, "UF 18.000", "Isidora Goyenechea, Las Condes, Santiago", "3 hab | 3 baños | 220 m² | Penthouse")
+    Casa(1, "UF 28.500", "Vitacura, Santiago", "4 hab | 5 baños | 450 m²"),
+    Casa(2, "UF 35.000", "Lo Barnechea, Santiago", "5 hab | 6 baños | 600 m² | Piscina"),
+    Casa(3, "UF 26.500", "Las Condes, Santiago", "4 hab | 4 baños | 500 m² | Jardín amplio"),
+    Casa(4, "UF 18.000", "Las Condes, Santiago", "3 hab | 3 baños | 220 m² | Penthouse")
 )
